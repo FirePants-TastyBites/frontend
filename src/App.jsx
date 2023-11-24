5e5814cbab3f171c3a781934643b34190443f59c
Shahin Farzane <42602105+Shahfarzane@users.noreply.github.com>
2023-11-24 09:40:17 +0100

    Sign-in page component done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Menu from "./pages/Menu";
 import Footer from "./components/Footer";
+import SignInPage from "./pages/Sign-in";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
             <Route path="/menu" element={<Menu />}></Route>
+            <Route path="/sign-in" element={<SignInPage />}></Route>
           </Routes>
         </div>
 
