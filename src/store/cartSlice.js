c54b06445b5a0ab286f2d06f9cc3158947efe4f8
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-11-27 09:34:53 +0100

    Setup for RTK

---
+++ src/store/cartSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItem: (state, action) => {
+            // Payload: complete item object
+            state.push(action.payload);
+        },
+        removeItem: (state, action) => {
+            // Payload: item id
+            const index = state.findIndex(item => item.id === action.payload.id);
+            state.splice(index, 1);
+        }
+    }
+});
+
+export const { addItem, removeItem } = cartSlice.actions;
+
+export default cartSlice.reducer;
