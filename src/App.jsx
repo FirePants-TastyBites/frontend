4c83f6514bad3b09db7056704eaac83c4779207c
Shahin Farzane <42602105+Shahfarzane@users.noreply.github.com>
2023-11-28 19:25:27 +0100

    Revert " Staffs orders handling page DONE"

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,8 @@ import Menu from "./pages/Menu";
 import Footer from "./components/Footer";
 import SignInPage from "./pages/Sign-in";
 import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
 import StaffPage from "./pages/Staff";
-import StaffLayout from "./components/StaffLayout";
-import StaffOrdersPage from "./pages/StaffOrders";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
@@ -19,17 +17,13 @@ function App() {
         <Header />
         <div className="content">
           <Routes>
- <Route path="/" element={<Home />}></Route>
+            <Route path="/" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
             <Route path="/menu" element={<Menu />}></Route>
             <Route path="/sign-in" element={<SignInPage />}></Route>
             <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<Checkout />} />
-
-  <Route path="/staff" element={<StaffLayout />}>
-              <Route index element={<StaffPage />} />
-              <Route path="orders" element={<StaffOrdersPage />} />
-
+            <Route path="/staff" element={<StaffPage />}></Route>
           </Routes>
         </div>
 
