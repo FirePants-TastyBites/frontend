2e21befb75a07de149cee7cd28e857c29987272b
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-12-05 14:27:00 +0100

    Sending userId in location state

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,8 +1,14 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import GreenLine from "../../components/GreenLine";
 import './Profile.scss';
 
 function Profile() {
+    const user = useLocation().state.user;
+    const navigate = useNavigate();
+
+    console.log(user);
+
     return (
         <main className="profile">
             <header>
@@ -14,8 +20,8 @@ function Profile() {
                 <p>
                     Welcome back! Check out your order history, keep tabs on your culinary adventures, and snag exclusive deals. It's your space—explore, enjoy, and let's make your dining experience awesome!
                 </p>
-                <Button label={"View Your Orders"} type={"primary"} />
-                <Button label={"Order Here"} type={"secondary"} />
+                <Button label={"View Your Orders"} type={"primary"} onClick={() => navigate('/order-history', { state: { user }})}/>
+                <Button label={"Order Here"} type={"secondary"} onClick={() => navigate('/menu')}/>
             </section>
             <figure>
                 <img src="https://images.unsplash.com/photo-1519671282429-b44660ead0a7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
