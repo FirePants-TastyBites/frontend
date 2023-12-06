86ba844690120898936ae9f35d4656eb771ee781
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-12-06 10:04:29 +0100

    Fetching and adding to right category

--- src/pages/OrderHistory/index.jsx
+++ src/pages/OrderHistory/index.jsx
@@ -1,11 +1,16 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import GreenLine from "../../components/GreenLine";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function OrderHistory() {
     const user = useLocation().state?.user || null;
     const navigate = useNavigate();
+    const [orderHistory, setOrderHistory] = useState({
+        inProgress: [],
+        delivered: [],
+        cancelled: []
+    })
 
     useEffect(() => {
         if (!user) {
@@ -15,10 +20,49 @@ function OrderHistory() {
 
     useEffect(() => {
         axios.post('https://gcr5ddoy04.execute-api.eu-north-1.amazonaws.com/user/orders', { userId: user })
-            .then(response => console.log(response))
+            .then(response => {
+                const orders = response.data.orders;
+                console.log("orders: ", orders)
+                orders.forEach(order => {
+
+                    if (order.orderStatus === 'cancelled') {
+                        setOrderHistory(prevHistory => ({...prevHistory, cancelled: [...prevHistory.cancelled, order]}));
+
+                    } else if (isDelivered(order.deliveryTime, order.createdAt)) {
+                        setOrderHistory(prevHistory => ({...prevHistory, delivered: [...prevHistory.delivered, order]}));
+
+                    } else {
+                        setOrderHistory(prevHistory => ({...prevHistory, inProgress: [...prevHistory.inProgress, order]}));
+                        
+                    }
+
+                });
+            })
             .catch(error => console.log(error))
     }, [])
 
+    function isDelivered(deliveryTime, createdAt) {
+
+        // Handle cancelled orders
+        if (!deliveryTime) {
+            return false;
+        }
+
+        // Turn createdAt into ms
+        const createdAtInMs = Date.parse(createdAt);
+        
+        // Extract date
+        const createdAtDate = createdAt.split('T')[0]
+
+        // Create valid Date for delivery and parse into ms
+        const deliveryAt = new Date(`${createdAtDate} ${deliveryTime}`);
+        const deliveryAtInMs = Date.parse(deliveryAt);
+
+        return deliveryAtInMs > Date.now();
+    }
+
+
+    console.log(orderHistory);
 
     return (
         <main>
@@ -34,15 +78,15 @@ function OrderHistory() {
             <section>
                 <section>
                     <h3>In progress</h3>
-                    <ul></ul>
+                    <ul>{orderHistory.inProgress.length}</ul>
                 </section>
                 <section>
                     <h3>Delivered</h3>
-                    <ul></ul>
+                    <ul>{orderHistory.delivered.length}</ul>
                 </section>
                 <section>
                     <h3>Cancelled</h3>
-                    <ul></ul>
+                    <ul>{orderHistory.cancelled.length}</ul>
                 </section>
             </section>
         </main>
